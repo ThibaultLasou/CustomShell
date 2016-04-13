8c08a6b3467cfc986011861983cc6fd67d5d3fa2
Thibault Lasou <tlasou@localhost.localdomain>
2016-04-13 14:12:25 +0200

    Bidouillage de trucs inutiles (machin -> machine)

--- main.c
+++ main.c
@@ -10,34 +10,35 @@
 
 #include "cmds.h"
 
-void printPrefix(struct utsname machin)
+void printPrefix(struct utsname machine)
 {
 	if(strncmp(getenv("PWD"), getenv("HOME"), strlen(getenv("HOME"))) == 0)
 	{
-		printf("%s@%s:~%s >> ", getenv("LOGNAME"), machin.nodename, getenv("PWD")+strlen(getenv("HOME")));
+		printf("%s@%s:~%s >> ", getenv("LOGNAME"), machine.nodename, getenv("PWD")+strlen(getenv("HOME")));
 	}
 	else
 	{
-		printf("%s@%s:%s >> ", getenv("LOGNAME"), machin.nodename, getenv("PWD"));
+		printf("%s@%s:%s >> ", getenv("LOGNAME"), machine.nodename, getenv("PWD"));
 	}
 }
 
 int main(int argc, char **argv, char *envp[])
 {
 	char buffer[BUF_SIZE];
-	char *loginName, *machineName, *histPath;
+	char *loginName, *histPath;
 	char **newArgv;
 	int newArgc, res;
 	pid_t pid;
-	struct utsname machin;
+	struct utsname machine;
 	FILE *hist;
 
 	histPath = malloc(sizeof(char)*(strlen(getenv("HOME"))+strlen("/.history")+1));
 	sprintf(histPath,"%s/.history", getenv("HOME"));
-	printf("%s", histPath);
-	uname(&machin);
+
+	uname(&machine);
+	
 	hist = fopen(histPath,"a+");
-	printPrefix(machin);
+	printPrefix(machine);
 	while(fgets(buffer, BUF_SIZE, stdin) != NULL)
 	{
 		fprintf(hist, buffer);
@@ -77,7 +78,7 @@ int main(int argc, char **argv, char *envp[])
 					break;
 			}
 		}
-		printPrefix(machin);
+		printPrefix(machine);
 	}
 	printf("\n");
 	return 0;
